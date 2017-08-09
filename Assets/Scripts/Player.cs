8f1cc9cc838de69c547706395e627ede55b4c7b7
damoch <damochramowicz260@gmail.com>
2017-08-09 12:25:49 +0200

    Basic of unit movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public List<Unit> Units;
+        private void Start()
+        {
+            
+        }
+        private void Update()
+        {
+            if (Input.GetMouseButton(button: (int)MouseButton.RightMouse))
+            {
+                foreach (var unit in Units)
+                {
+                    Debug.Log("hahah");
+                    var target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    target.z = 0f;
+                    unit.SetNewTarget(target);
+                }
+            }
+        }
+    
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Unit : MonoBehaviour
+    {
+        private Vector3 _target;
+        
+        private void Start ()
+        {
+            _target = transform.position;
+        }
+	
+        private void Update () {
+            if (ShouldMove())
+            {
+                MoveTowardsTarget();
+            }
+		
+        }
+
+        private void MoveTowardsTarget()
+        {
+            transform.Translate(_target);
+        }
+
+        private bool ShouldMove()
+        {
+            return !_target.Equals(transform.position);
+        }
+
+        public void SetNewTarget(Vector3 newTarget)
+        {
+            _target = newTarget;
+        }
+        
+    }
+}
