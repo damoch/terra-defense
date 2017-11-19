8d43eabfeb1b08141eed778a9503c3f33629974f
Damian Ochramowicz <damochramowicz260@gmail.com>
2017-11-19 15:41:17 +0100

    map generating part 3

--- Assets/TerraDefense/Implementations/Controllers/GameController.cs
+++ Assets/TerraDefense/Implementations/Controllers/GameController.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Assets.TerraDefense.Implementations.Factions;
 using Assets.TerraDefense.Implementations.Players;
 using Assets.TerraDefense.Implementations.Units;
 using Assets.TerraDefense.Implementations.World;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Assets.TerraDefense.Implementations.Controllers
 {
@@ -17,39 +19,55 @@ namespace Assets.TerraDefense.Implementations.Controllers
         public GameObject CountryGameObject;
         public GameObject ProvinceGameObject;
         public GameObject AlienPlatformGameObject;
-        public float OriginX;
-        public float OriginY;
+        public int MapSquareWidth;
 
         public int NumberOfCountries;
-        public int ProvincesPerCountry;
         public int NumberOfInvaders;
-
-        private Vector2 _pointOfOrigin;
+        
         private float _provinceHeight;
         private float _provinceWidth;
         private List<Province> _generatedProvinces;
+        private int _provincesPerCountry;
+        private Dictionary<int, Dictionary<int, Province>> _provincesMap;
 
         public Player Player { get; set; }
         public Alliance Alliance { get; set; }
         public Aliens Aliens { get; set; }
         public Clock Clock { get; set; }
-        public AIPlayer AIPlayer { get; set; }
+        public AIPlayer AiPlayer { get; set; }
         private void Start () {
-            _pointOfOrigin = new Vector2(OriginX, OriginY);
             _provinceHeight = ProvinceGameObject.GetComponent<BoxCollider2D>().size.y * ProvinceGameObject.transform.localScale.y;
             _provinceWidth = ProvinceGameObject.GetComponent<BoxCollider2D>().size.x * ProvinceGameObject.transform.localScale.x;
+            _provincesMap = new Dictionary<int, Dictionary<int, Province>>();
+            _provincesPerCountry = Convert.ToInt32(Math.Pow(MapSquareWidth, 2) / NumberOfCountries);
 
             Initialize();
+            CreateProvincesMap();
             CreateCountries();
             Invoke("CreateInvaders", 2f);
         }
 
+        private void CreateProvincesMap()
+        {
+            for (var x = 0; x < MapSquareWidth; x++)
+            {
+                _provincesMap.Add(x, new Dictionary<int, Province>());
+                for (var y = 0; y < MapSquareWidth; y++)
+                {
+                    var province = Instantiate(ProvinceGameObject).GetComponent<Province>();
+                    province.gameObject.transform.position = new Vector2(x * _provinceWidth, -y * _provinceHeight);
+                    _provincesMap[x].Add(y, province);
+                }
+            }
+        }
+
         private void CreateInvaders()
         {
-            for (int i = 0; i < NumberOfInvaders; i++)
+            for (var i = 0; i < NumberOfInvaders; i++)
             {
-                var index = Random.Range(0, _generatedProvinces.Count);
-                var province = _generatedProvinces[index];
+                var x = Random.Range(0, MapSquareWidth);
+                var y = Random.Range(0, MapSquareWidth);
+                var province = _provincesMap[x][y];
                 var platform = Instantiate(AlienPlatformGameObject).GetComponent<PlatformUnit>();
                 platform.AliensOwner = Aliens;
                 platform.transform.position = province.transform.position;
@@ -59,24 +77,38 @@ namespace Assets.TerraDefense.Implementations.Controllers
         private void CreateCountries()
         {
             _generatedProvinces = new List<Province>();
-
+            var startX = 0;
+            var startY = 0;
+            var countryWidth = Convert.ToInt32(Math.Sqrt(_provincesPerCountry));
 
             for (var i = 0; i < NumberOfCountries; i++)
             {
-                var color = new Color(Random.Range(0,255)/(float)255, Random.Range(0, 1) / (float)255, Random.Range(0, 1) / (float)255);
-                var unitColor = new Color(Random.Range(0, 255) / (float)255, Random.Range(0, 1) / (float)255, Random.Range(0, 1) / (float)255);
+                var color = new Color(Random.Range(0,255)/(float)255, Random.Range(0, 255) / (float)255, Random.Range(0, 255) / (float)255);
+                var unitColor = new Color(Random.Range(0, 255) / (float)255, Random.Range(0, 255) / (float)255, Random.Range(0, 255) / (float)255);
 
                 var country = Instantiate(CountryGameObject).GetComponent<Country>();
                 country.Color = color;
                 country.UnitColor = unitColor;
                 country.Name = "Country" + i;
+#if UNITY_EDITOR
+                country.gameObject.name = "Country" + i;
+#endif
                 country.Alliance = Alliance;
-                for (var j = 0; j < ProvincesPerCountry; j++)
+
+                for (var x = startX; x < startX + countryWidth; x++)
                 {
-                    var province = Instantiate(ProvinceGameObject).GetComponent<Province>();
-                    province.gameObject.transform.position = new Vector2(j * _provinceWidth, i * _provinceHeight);
-                    province.Owner = country;
-                    _generatedProvinces.Add(province);
+                    for (var y = startY; y < startY + countryWidth; y++)
+                    {
+                        _provincesMap[x][y].Owner = country;
+                        _provincesMap[x][y].enabled = true;
+                    }
+                    
+                }
+                if (startX < MapSquareWidth - startX) startX += countryWidth;
+                else
+                {
+                    startX = 0;
+                    startY += countryWidth;
                 }
             }
 
@@ -85,11 +117,20 @@ namespace Assets.TerraDefense.Implementations.Controllers
         private void Initialize()
         {
             Player = Instantiate(PlayerGameObject).GetComponent<Player>();
-            AIPlayer = Instantiate(AiPlayerGameObject).GetComponent<AIPlayer>();
+            AiPlayer = Instantiate(AiPlayerGameObject).GetComponent<AIPlayer>();
             Clock = Instantiate(ClockGameObject).GetComponent<Clock>();
             Aliens = Instantiate(AliensGameObject).GetComponent<Aliens>();
-            AIPlayer.Aliens = Aliens;
+            AiPlayer.Aliens = Aliens;
             Alliance = Instantiate(AllianceGameObject).GetComponent<Alliance>();
+        }
+
+        private bool IsPowerOfTwo(int x)
+        {
+            while (((x % 2) == 0) && x > 1)
+            {
+                x /= 2;
+            }
+            return (x == 1);
         }
     }
 }
